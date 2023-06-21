bbdc7a3797a2efd0daad4aee56c446fe9bf4f248
manoel-linux <135872910+manoel-linux@users.noreply.github.com>
2023-06-21 16:39:30 -0300

    Update installupdate.sh

--- installupdate.sh
+++ installupdate.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+clear
 
+show_main_menu() {
+while true; do
 clear
 echo "#################################################################"
 echo "way-display-guard-installer: june 2023"
@@ -14,6 +17,7 @@ echo "#################################################################"
 echo "way-display-guard-github: https://github.com/manoel-linux/way-display-guard"
 echo "#################################################################"
 
+
 if [[ $EUID -ne 0 ]]; then
 echo " ███████ ██████  ██████   ██████  ██████  ██ "
 echo " ██      ██   ██ ██   ██ ██    ██ ██   ██ ██ "
@@ -26,6 +30,60 @@ echo "#################################################################"
 exit 1
 fi
 
+echo "(1)> Install the way-display-guard version of Void-Linux"
+echo "(2)> Install the way-display-guard version of Ubuntu/Debian"
+echo "(3)> Install the way-display-guard version of Arch-Artix-Manjaro"
+echo "(4)> Exit"
+echo "#################################################################"
+
+read -p "Enter your choice: " choice
+echo "#################################################################"
+
+case $choice in
+1)
+show_void-linux
+;;
+2)
+show_ubuntu-debian
+;;
+3)
+show_arch-artix-manjaro
+;;
+4)
+exit 0
+;;
+*)
+echo "Invalid choice. Please try again."
+echo "#################################################################"
+sleep 2
+;;
+esac
+done
+}
+
+show_void-linux() {
+while true; do
+clear
+echo "#################################################################"
+echo "Checking for updates in Void Linux..." 
+echo "#################################################################"
+if ping -q -c 1 -W 1 voidlinux.org >/dev/null; then
+echo "Internet connection established. It is possible to check for updates."
+echo "#################################################################"
+
+read -p "Do you want to update your system? (y/n): " choice
+echo "#################################################################"
+if [[ $choice == "y" || $choice == "Y" ]]; then
+sudo xbps-install -Syu -y
+else
+echo "Skipping system update."
+echo "#################################################################"
+sudo xbps-install glxinfo unzip binutils tar curl xbps xz lm_sensors dialog -y
+fi
+else
+echo "#################################################################"
+echo "No internet connection. Unable to check for updates Skipping."
+fi
 sudo rm /usr/bin/way-display-guard
 
 sudo cp way-display-guard /usr/bin/
@@ -42,3 +100,119 @@ echo "Installation/Update completed."
 echo "#################################################################"
 echo "To use way-display-guard, execute the following command: way-display-guard"
 echo "#################################################################"
+read -rsn1 -p "press Enter to return to the main menu
+#################################################################" key
+if [[ $key == "r" || $key == "R" ]]; then
+continue
+fi
+
+break
+done
+
+echo "#################################################################"
+}
+
+show_ubuntu-debian() {
+while true; do
+clear
+echo "#################################################################"
+echo "Checking for updates in Ubuntu/Debian..." 
+echo "#################################################################"
+if ping -q -c 1 -W 1 debian.org >/dev/null; then
+echo "Internet connection established. It is possible to check for updates."
+echo "#################################################################"
+
+read -p "Do you want to update your system? (y/n): " choice
+echo "#################################################################"
+if [[ $choice == "y" || $choice == "Y" ]]; then
+sudo apt update -y
+sudo apt upgrade -y
+else
+echo "Skipping system update."
+echo "#################################################################"
+sudo apt install mesa-utils unzip binutils tar curl xz-utils usbutils grep gawk sed lm-sensors dialog -y
+fi
+else
+echo "#################################################################"
+echo "No internet connection. Unable to check for updates Skipping."
+fi
+sudo rm /usr/bin/way-display-guard
+
+sudo cp way-display-guard /usr/bin/
+
+sudo chmod +x /usr/bin/way-display-guard
+
+echo " ██████   ██████  ███    ██ ███████ ██ "
+echo " ██   ██ ██    ██ ████   ██ ██      ██ "
+echo " ██   ██ ██    ██ ██ ██  ██ █████   ██ "
+echo " ██   ██ ██    ██ ██  ██ ██ ██         "
+echo " ██████   ██████  ██   ████ ███████ ██ "  
+echo "#################################################################"
+echo "Installation/Update completed."
+echo "#################################################################"
+echo "To use way-display-guard, execute the following command: way-display-guard"
+echo "#################################################################"
+read -rsn1 -p "press Enter to return to the main menu
+#################################################################" key
+if [[ $key == "r" || $key == "R" ]]; then
+continue
+fi
+
+break
+done
+
+echo "#################################################################"
+}
+
+show_arch-artix-manjaro() {
+while true; do
+clear
+echo "#################################################################"
+echo "Checking for updates in Arch/Artix/Manjaro..." 
+echo "#################################################################"
+if ping -q -c 1 -W 1 archlinux.org >/dev/null; then
+echo "Internet connection established. It is possible to check for updates."
+echo "#################################################################"
+
+read -p "Do you want to update your system? (y/n): " choice
+echo "#################################################################"
+if [[ $choice == "y" || $choice == "Y" ]]; then
+sudo pacman -Syu -y
+else
+echo "Skipping system update."
+echo "#################################################################"
+sudo pacman -S mesa-utils glxinfo mesa-demos glxinfo unzip binutils tar curl xz usbutils grep gawk sed lm_sensors dialog -y
+fi
+else
+echo "#################################################################"
+echo "No internet connection. Unable to check for updates Skipping."
+fi
+sudo rm /usr/bin/way-display-guard
+
+sudo cp way-display-guard /usr/bin/
+
+sudo chmod +x /usr/bin/way-display-guard
+
+echo " ██████   ██████  ███    ██ ███████ ██ "
+echo " ██   ██ ██    ██ ████   ██ ██      ██ "
+echo " ██   ██ ██    ██ ██ ██  ██ █████   ██ "
+echo " ██   ██ ██    ██ ██  ██ ██ ██         "
+echo " ██████   ██████  ██   ████ ███████ ██ "  
+echo "#################################################################"
+echo "Installation/Update completed."
+echo "#################################################################"
+echo "To use way-display-guard, execute the following command: way-display-guard"
+echo "#################################################################"
+read -rsn1 -p "press Enter to return to the main menu
+#################################################################" key
+if [[ $key == "r" || $key == "R" ]]; then
+continue
+fi
+
+break
+done
+
+echo "#################################################################"
+}
+
+show_main_menu
