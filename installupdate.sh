003f703a8f29eccd55edae9e4706dfaec7dbb9f3
manoel-linux <135872910+manoel-linux@users.noreply.github.com>
2023-08-22 01:36:21 -0300

    Update installupdate.sh

--- installupdate.sh
+++ installupdate.sh
@@ -6,7 +6,7 @@ show_main_menu() {
 while true; do
 clear
 echo "#################################################################"
-echo "way-display-guard-installer >> (aug 2023)"
+echo "(way-display-guard-installer) >> (aug 2023)"
 echo "#################################################################"
 echo " ██     ██  █████  ██    ██ "
 echo " ██     ██ ██   ██  ██  ██  "
@@ -14,7 +14,7 @@ echo " ██  █  ██ ███████   ████   "
 echo " ██ ███ ██ ██   ██    ██    "
 echo "  ███ ███  ██   ██    ██    "    
 echo "#################################################################"
-echo "way-display-guard-github >> (https://github.com/manoel-linux/way-display-guard)"
+echo "(way-display-guard-github) >> (https://github.com/manoel-linux/way-display-guard)"
 echo "#################################################################"
 
 if [[ $EUID -ne 0 ]]; then
@@ -24,7 +24,7 @@ echo " █████   ██████  ██████  ██    █�
 echo " ██      ██   ██ ██   ██ ██    ██ ██   ██    "
 echo " ███████ ██   ██ ██   ██  ██████  ██   ██ ██ "                                                                                        
 echo "#################################################################"
-echo "Superuser privileges or sudo required to execute the script." 
+echo "(Superuser privileges or sudo required to execute the script)" 
 echo "#################################################################"
 exit 1
 fi
@@ -42,13 +42,13 @@ echo "#################################################################"
 clear
 
 echo "#################################################################"
-echo "(1)> (Install) the way-display-guard version of Void-Linux"
-echo "(2)> (Install) the way-display-guard version of Ubuntu/Debian"
-echo "(3)> (Install) the way-display-guard version of Arch-Artix-Manjaro"
+echo "(1)> (Install) >> (the the way-display-guard version of Void-Linux)"
+echo "(2)> (Install) >> (the the way-display-guard version of Ubuntu/Debian)"
+echo "(3)> (Install) >> (the the way-display-guard version of Arch-Artix-Manjaro)"
 echo "(4)> (Exit)"
 echo "#################################################################"
 
-read -p "Enter your choice: " choice
+read -p "(Enter your choice) >> " choice
 echo "#################################################################"
 
 case $choice in
@@ -65,7 +65,7 @@ show_arch-artix-manjaro
 exit 0
 ;;
 *)
-echo "Invalid choice. Please try again."
+echo "(Invalid choice. Please try again)"
 echo "#################################################################"
 sleep 2
 ;;
@@ -78,25 +78,25 @@ while true; do
 clear
 if [ ! -x /bin/xbps-install ]; then
 echo "#################################################################"
-echo "(Warning!) >> You are trying to run a version meant for another distribution. 
-To prevent issues, the script has blocked a warning to execute the version meant for your distribution."
+echo "(Warning) >> (You are trying to run a version meant for another distribution) 
+(To prevent issues, the script has blocked a warning to execute the version meant for your distribution)"
 echo "#################################################################"
 exit 1
 fi
-echo "Checking for updates in Void Linux..." 
+echo "(Checking for updates in Void Linux)" 
 echo "#################################################################"
 sudo xbps-install -Sy
 sudo xbps-install -S unzip binutils tar curl xbps xz grep gawk sed -y
 clear
 echo "#################################################################"
 
-read -p "Do you want to update your system? (y/n): " choice
+read -p "(Do you want to update your system) (y/n) >> " choice
 echo "#################################################################"
 if [[ $choice == "y" || $choice == "Y" ]]; then
 sudo xbps-install -Sy
 sudo xbps-install -Syu -y
 else
-echo "Skipping system update."
+echo "(Skipping system update)"
 echo "#################################################################"
 fi
 
@@ -119,11 +119,11 @@ echo " ██   ██ ██    ██ ██ ██  ██ █████   
 echo " ██   ██ ██    ██ ██  ██ ██ ██         "
 echo " ██████   ██████  ██   ████ ███████ ██ "  
 echo "#################################################################"
-echo "Installation/Update completed."
+echo "(Installation/Update completed)"
 echo "#################################################################"
 echo "To use way-display-guard, execute the following command: way-display-guard"
 echo "#################################################################"
-read -rsn1 -p "press Enter to return to the main menu
+read -rsn1 -p "(press Enter to return to the main menu)
 #################################################################" key
 if [[ $key == "r" || $key == "R" ]]; then
 continue
@@ -140,26 +140,26 @@ while true; do
 clear
 if [ ! -x /bin/apt ]; then
 echo "#################################################################"
-echo "(Warning!) >> You are trying to run a version meant for another distribution. 
-To prevent issues, the script has blocked a warning to execute the version meant for your distribution."
+echo "(Warning) >> (You are trying to run a version meant for another distribution) 
+(To prevent issues, the script has blocked a warning to execute the version meant for your distribution)"
 echo "#################################################################"
 exit 1
 fi
 echo "#################################################################"
-echo "Checking for updates in Ubuntu/Debian..." 
+echo "(Checking for updates in Ubuntu/Debian)" 
 echo "#################################################################"
 sudo apt-get update
 sudo apt-get install --no-install-recommends unzip binutils tar curl xz-utils grep gawk sed -y
 clear
 echo "#################################################################"
 
-read -p "Do you want to update your system? (y/n): " choice
+read -p "(Do you want to update your system) (y/n) >> " choice
 echo "#################################################################"
 if [[ $choice == "y" || $choice == "Y" ]]; then
 sudo apt-get update
 sudo apt-get upgrade -y
 else
-echo "Skipping system update."
+echo "(Skipping system update)"
 echo "#################################################################"
 fi
 
@@ -182,11 +182,11 @@ echo " ██   ██ ██    ██ ██ ██  ██ █████   
 echo " ██   ██ ██    ██ ██  ██ ██ ██         "
 echo " ██████   ██████  ██   ████ ███████ ██ "  
 echo "#################################################################"
-echo "Installation/Update completed."
+echo "(Installation/Update completed)"
 echo "#################################################################"
 echo "To use way-display-guard, execute the following command: way-display-guard"
 echo "#################################################################"
-read -rsn1 -p "press Enter to return to the main menu
+read -rsn1 -p "(press Enter to return to the main menu)
 #################################################################" key
 if [[ $key == "r" || $key == "R" ]]; then
 continue
@@ -200,28 +200,29 @@ echo "#################################################################"
 
 show_arch-artix-manjaro() {
 while true; do
+clear
 if [ ! -x /bin/pacman ]; then
 echo "#################################################################"
-echo "(Warning!) >> You are trying to run a version meant for another distribution. 
-To prevent issues, the script has blocked a warning to execute the version meant for your distribution."
+echo "(Warning) >> (You are trying to run a version meant for another distribution) 
+(To prevent issues, the script has blocked a warning to execute the version meant for your distribution)"
 echo "#################################################################"
 exit 1
 fi
 echo "#################################################################"
-echo "Checking for updates in Arch/Artix/Manjaro..." 
+echo "(Checking for updates in Arch/Artix/Manjaro)" 
 echo "#################################################################"
 sudo pacman -Sy
 sudo pacman -S unzip binutils tar curl xz grep gawk sed -y
 clear
 echo "#################################################################"
 
-read -p "Do you want to update your system? (y/n): " choice
+read -p "(Do you want to update your system) (y/n) >> " choice
 echo "#################################################################"
 if [[ $choice == "y" || $choice == "Y" ]]; then
 sudo pacman -Sy
 sudo pacman -Syu -y
 else
-echo "Skipping system update."
+echo "(Skipping system update)"
 echo "#################################################################"
 fi
 
@@ -244,11 +245,11 @@ echo " ██   ██ ██    ██ ██ ██  ██ █████   
 echo " ██   ██ ██    ██ ██  ██ ██ ██         "
 echo " ██████   ██████  ██   ████ ███████ ██ "  
 echo "#################################################################"
-echo "Installation/Update completed."
+echo "(Installation/Update completed)"
 echo "#################################################################"
 echo "To use way-display-guard, execute the following command: way-display-guard"
 echo "#################################################################"
-read -rsn1 -p "press Enter to return to the main menu
+read -rsn1 -p "(press Enter to return to the main menu)
 #################################################################" key
 if [[ $key == "r" || $key == "R" ]]; then
 continue
